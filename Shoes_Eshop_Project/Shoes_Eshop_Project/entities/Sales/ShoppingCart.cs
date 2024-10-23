541b1bd10577e6900fcc98c85dcabb589267b055
s28470 <s28470@pjwstk.edu.pl>
2024-10-23 14:52:57 +0200

    Shopping cart methods

--- Shoes_Eshop_Project/Shoes_Eshop_Project/entities/Sales/ShoppingCart.cs
+++ Shoes_Eshop_Project/Shoes_Eshop_Project/entities/Sales/ShoppingCart.cs
@@ -4,9 +4,11 @@ public class ShoppingCart
 {
     public Dictionary<Product, int> Products{get; init;}
     
-    private bool _isCompleted;
+    private bool _isCompleted = false;
 
-    private decimal _totalPrice;
+    private decimal _totalPrice = 0;
+
+    public Customer Customer { get; set; }
 
     public ShoppingCart()
     {
@@ -19,4 +21,25 @@ public class ShoppingCart
         _totalPrice = Products.Select(productAndAmount => productAndAmount.Key.Price * productAndAmount.Value).Sum();
         return _totalPrice;
     }
+
+    public void AddProduct(Product product, int amount)
+    {
+        if (amount <= 0)
+        {
+            return;
+        }
+
+        if (Products.ContainsKey(product))
+        {
+            Products[product] += amount;
+        }
+        else
+        {
+            Products[product] = amount;
+        }
+        
+        
+    }
+    
+    
 }
